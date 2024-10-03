a21c601bd207d9546bc446fd429870d9ed76dabd
JuanGarciaAguilar <Juan.Garcia.Dev.94@gmail.com>
2024-10-03 10:06:24 -0500

    prueba

--- Frontend/src/app/Components/Reportes/ReporteDiario/ReporteDiario.component.ts
+++ Frontend/src/app/Components/Reportes/ReporteDiario/ReporteDiario.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { ClienteModel } from 'src/app/Shared/Models/ClienteModel';
 import { CierreDiarioReportHeader } from 'src/app/Shared/Models/ReportesModel';
 import { ClienteService } from 'src/app/Shared/Service/Cliente.service';
 import { ReportesService } from 'src/app/Shared/Service/Reportes.service';
@@ -15,37 +16,108 @@ export class ReporteDiarioComponent implements OnInit {
         { icon: 'pi pi-home', route: '/' },
         { label: 'Reporte diario' },
     ];
- /*    private _ClienteService = inject(ClienteService);
+    private _ClienteService = inject(ClienteService);
     private _SectorService = inject(SectorService);
-    private _ReportesService = inject(ReportesService); */
+    private _ReportesService = inject(ReportesService);
     constructor() {}
     sales: any;
     SectorData: any;
+    ClientesData:ClienteModel[]=[];
     ngOnInit() {
-        /* this.GetSectores();
-        this.GetClientes();
-        this.getReporteCierre DiarioCabecera();*/
+         this.GetSectores();
+          this.GetClientes();
+       this.getReporteCierreDiarioCabecera();
     }
 
-   /*  GetClientes() {
-        this._ClienteService.getListaClientes().subscribe((clientesData) => {
-            console.log(clientesData);
-        });
-    }
 
     GetSectores() {
         this._SectorService.getListaSector().subscribe((data: any) => {
             this.SectorData = data;
+            console.log('sectores',data);
+
         });
     }
 
+    GetClientes() {
+        this._ClienteService.getListaClientes().subscribe((clientesData) => {
+
+            this.ClientesData = clientesData;
+        });
+    }
+
+
+
     columns: CierreDiarioReportHeader[] = [];
     getReporteCierreDiarioCabecera() {
-        this._ReportesService
-            .getReporteCierreDiarioCabecera()
-            .subscribe((data: any) => {
-                this.columns = data;
-                console.log(data);
+        this._ReportesService.getReporteCierreDiarioCabecera().subscribe((data: any) => {
+            this.columns = data;
+                console.log('columnas',data);
             });
-    } */
+    }
+
+    FechaInicio:string = '';
+    SectorSelected :any;
+    clientes: ClienteModel[] = [];
+    GetReporte(){
+        this._ReportesService
+        .getReporteCierreDiario(this.FechaInicio, this.SectorSelected)
+        .subscribe((reporteData:any) => {
+
+        /*   this.totalSaldoAnterior = 0;
+          this.totalVenta = 0;
+          this.totalAmortizacion = 0;
+   */debugger
+          this.clientes = this.ClientesData
+            .filter((x:any) => x.sectorId == this.SectorSelected)
+            .sort((a:any, b:any) => {
+              const x = a.clienteName.toLowerCase();
+              const y = b.clienteName.toLowerCase();
+              return x < y ? -1 : x > y ? 1 : 0;
+            });
+            console.log('clientes',this.ClientesData);
+            console.log('clientes del reporte',this.clientes);
+
+
+         /*  reporteData
+            .filter((x) => x.productParentName == null)
+            .forEach((x) => {
+              this.totalSaldoAnterior =
+                this.totalSaldoAnterior + x.deudaActualizada;
+              this.totalVenta = this.totalVenta + x.total;
+              this.totalAmortizacion = this.totalAmortizacion + x.amortizacion;
+              this.saldo =
+                this.totalSaldoAnterior +
+                this.totalVenta -
+                this.totalAmortizacion;
+            }); */
+
+            console.log('datos del reporte',reporteData);
+
+       //   this.reporteCierreDiarioKendo = reporteData;
+/*
+          this.clientes.forEach((cli) => {
+            this.kendoHeaderGroupReordenar.forEach((k) => {
+              k.hijos.forEach((h) => {
+                const data = reporteData.filter(
+                  (r) =>
+                    r.productParentName == h.title && r.clienteId == cli.clienteId
+                );
+
+                const cant = data
+                  .filter((r) => r.unidadMedida == "Unidades")
+                  .reduce((a, b) => b.valor, 0);
+                const sol = data
+                  .filter((r) => r.unidadMedida == "Soles")
+                  .reduce((a, b) => b.valor, 0);
+
+                h.cant[cli.clienteId] = cant == 0 ? null : cant;
+                h.sol[cli.clienteId] = sol == 0 ? null : sol;
+              });
+            });
+          }); */
+
+
+          console.log("end");
+        });
+    }
 }
--- Frontend/src/app/Shared/Service/Reportes.service.ts
+++ Frontend/src/app/Shared/Service/Reportes.service.ts
@@ -3,13 +3,14 @@ import { inject, Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { GlobalConstants } from '../Models/GlobalConstants';
 import { CierreDiarioReportHeader, EstadoFinanciero } from '../Models/ReportesModel';
+import { CierreDiarioReport } from '../Models/cierre-diario.report';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportesService {
 
-    private _ReportesService = inject(ReportesService);
+
     private _HttpClient = inject(HttpClient);
     private _AuthService = inject(AuthService);
 
@@ -20,7 +21,11 @@ GetReporte(){
     return this._HttpClient.get<EstadoFinanciero[]>(GlobalConstants.ReporteStock + 'estadofinanciero');
 }
 
-getReporteCierreDiarioCabecera() {
-    return this._HttpClient.get<CierreDiarioReportHeader[]>(GlobalConstants.Ventas + 'ReporteCierreDiarioCabecera');
+getReporteCierreDiarioCabecera():any {
+    return this._HttpClient.get<CierreDiarioReportHeader[]>(GlobalConstants.Ventas + 'ReporteCierreDiarioCabecera',this._AuthService.getHeader());
+  }
+
+  getReporteCierreDiario(fIni:any, cSector:number) {
+    return this._HttpClient.get<CierreDiarioReport[]>(GlobalConstants.Ventas + 'ReporteCierreDiario/' + fIni + '/' + cSector);
   }
 }
